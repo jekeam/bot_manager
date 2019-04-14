1f53d028a7b01134802183e576ad14071a11d127
suineg <suinegne@inbox.ru>
2019-04-14 20:54:21 +0500

    add test

---
+++ test.py
@@ -0,0 +1,30 @@
+# coding:utf-8
+import subprocess
+import os
+
+os.chdir('D:\\YandexDisk\\Парсинг\\better')
+args = ['python', 'test.py']
+process = subprocess.Popen(args, stdout=subprocess.PIPE, shell=True)
+
+"""
+import os
+import subprocess
+import sys
+
+child = os.path.join(os.path.dirname(__file__), "./child.py")
+word = 'word'
+file = ['./parent.py', './child.py']
+
+pipes = []
+for i in range(0, 2):
+    command = [sys.executable, child]
+    pipe = subprocess.Popen(command, stdin=subprocess.PIPE)
+    pipes.append(pipe)
+    pipe.stdin.write(word.encode("utf8") + b"\n")
+    pipe.stdin.write(file[i].encode("utf8") + b"\n")
+    pipe.stdin.close()
+
+while pipes:
+    pipe = pipes.pop()
+    pipe.wait()
+"""
