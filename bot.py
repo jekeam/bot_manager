15802fe5e39b4f00cc10c98e911f4f7e5489015e
suineg <suinegne@inbox.ru>
2019-04-24 01:21:08 +0500

    set prop 50%

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ import os
 import datetime
 import time
 
-logging.basicConfig(filename='bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+# logging.basicConfig(filename='bot.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
