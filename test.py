5cb9faf81e6c4a52d47390ff6655db01e8a82bcd
suineg <suineg@inbox.ru>
2019-04-25 14:01:06 +0300

    middel res

--- test.py
+++ test.py
@@ -1,152 +1,39 @@
-# -*- coding: utf-8 -*-
-import bot_prop
+sum_bet_all = 400
+cur_val_bet = 1.55
+old_val_bet = 1.7
+k_opp = 2.5
+# 150
 
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
-                          ConversationHandler, PicklePersistence)
+bet1 = 280
 
-import logging
+profit = bet1*old_val_bet
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
-
-logger = logging.getLogger(__name__)
-
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
-
-reply_keyboard = [['Age', 'Favourite colour'],
-                  ['Number of siblings', 'Something else...'],
-                  ['Done']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+x = profit/bet1-cur_val_bet
+print(x)
 
 
-def facts_to_str(user_data):
-    facts = list()
 
-    for key, value in user_data.items():
-        facts.append('{} - {}'.format(key, value))
+# print('l: ' + str(proc))
+# print('l new: ' + str(proc2))
 
-    return "\n".join(facts).join(['\n', '\n'])
+# print(sum_bet_all*proc)
+# print(sum_bet_all*proc2)
 
-
-def start(update, context):
-    reply_text = "Hi! My name is Doctor Botter."
-    if context.user_data:
-        reply_text += " You already told me your {}. Why don't you tell me something more " \
-                      "about yourself? Or change enything I " \
-                      "already know.".format(", ".join(context.user_data.keys()))
-    else:
-        reply_text += " I will hold a more complex conversation with you. Why don't you tell me " \
-                      "something about yourself?"
-    update.message.reply_text(reply_text, reply_markup=markup)
-
-    return CHOOSING
+# new_l = (k_opp * cur_val_bet) / (k_opp + cur_val_bet)
+# old_l = (k_opp * old_val_bet) / (k_opp + old_val_bet)
 
 
-def regular_choice(update, context):
-    text = update.message.text
-    context.user_data['choice'] = text
-    if context.user_data.get(text):
-        reply_text = 'Your {}, I already know the following ' \
-                     'about that: {}'.format(text.lower(), context.user_data[text.lower()])
-    else:
-        reply_text = 'Your {}? Yes, I would love to hear about that!'.format(text.lower())
-    update.message.reply_text(reply_text)
+# print('new_l: ' + str(new_l))
+# print('old_l: ' + str(old_l))
 
-    return TYPING_REPLY
+# l = (1/cur_val_bet)+(1/k_opp)
 
+# round_rang = 5
+# #sum_bet = round(((sum_bet_all / new_l) / (cur_val_bet / new_l)) / round_rang) * round_rang
+# sum_bet = round((cur_val_bet / l) / (cur_val_bet*(1 / l)) / round_rang) * round_rang
+# print(sum_bet)
 
-def custom_choice(update, context):
-    update.message.reply_text('Alright, please send me the category first, '
-                              'for example "Most impressive skill"')
-
-    return TYPING_CHOICE
-
-
-def received_information(update, context):
-    text = update.message.text
-    category = context.user_data['choice']
-    context.user_data[category] = text.lower()
-    del context.user_data['choice']
-
-    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
-                              "{}"
-                              "You can tell me more, or change your opinion on "
-                              "something.".format(facts_to_str(context.user_data)),
-                              reply_markup=markup)
-
-    return CHOOSING
-
-
-def show_data(update, context):
-    update.message.reply_text("This is what you already told me:"
-                              "{}".format(facts_to_str(context.user_data)))
-
-
-def done(update, context):
-    if 'choice' in context.user_data:
-        del context.user_data['choice']
-
-    update.message.reply_text("I learned these facts about you:"
-                              "{}"
-                              "Until next time!".format(facts_to_str(context.user_data)))
-    return ConversationHandler.END
-
-
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, error)
-
-
-def main():
-    # Create the Updater and pass it your bot's token.
-    pp = PicklePersistence(filename='conversationbot')
-    updater = Updater(bot_prop.TOKEN, persistence=pp, use_context=True, request_kwargs=bot_prop.REQUEST_KWARGS)
-
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # Add conversation handler with the states CHOOSING, TYPING_CHOICE and TYPING_REPLY
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-
-        states={
-            CHOOSING: [RegexHandler('^(Age|Favourite colour|Number of siblings)$',
-                                    regular_choice),
-                       RegexHandler('^Something else...$',
-                                    custom_choice),
-                       ],
-
-            TYPING_CHOICE: [MessageHandler(Filters.text,
-                                           regular_choice),
-                            ],
-
-            TYPING_REPLY: [MessageHandler(Filters.text,
-                                          received_information),
-                           ],
-        },
-
-        fallbacks=[RegexHandler('^Done$', done)],
-        name="my_conversation",
-        persistent=True
-    )
-
-    dp.add_handler(conv_handler)
-
-    show_data_handler = CommandHandler('show_data', show_data)
-    dp.add_handler(show_data_handler)
-    # log all errors
-    dp.add_error_handler(error)
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+# #ROUND((K2/NEW%)/(K2*(1/NEW%));-1))
+# #K2 - новый коф
+# #Bet - сколько планировали ставить
+# #NEW% - новый процент вилки
