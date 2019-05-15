11ed5e3d43514f9fc7120a86cc83bf5332daa4c7
suineg <suineg@inbox.ru>
2019-05-15 11:51:07 +0500

    refact

--- test2.py
+++ test2.py
@@ -1,9 +1,9 @@
-import numpy as np
+# import numpy as np
 
-x = [1, 2, 3, 4]
+# x = [1, 2, 3, 4]
 
-xx = np.asarray(x).reshape(len(x), 1)
-yy = np.asarray(x).reshape(len(x), 1)
-n = zip(xx, yy)
+# xx = np.asarray(x).reshape(len(x), 1)
+# yy = np.asarray(x).reshape(len(x), 1)
+# n = zip(xx, yy)
 
-print(n[-1][1][0])
+# print(n[-1][1][0])
