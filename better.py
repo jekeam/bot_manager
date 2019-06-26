d94a3434961b48645340f52ffcce2f566448cc0b
suineg <suinegne@inbox.ru>
2019-06-27 00:24:07 +0500

    Update better.py

--- better.py
+++ better.py
@@ -71,8 +71,8 @@ def check_l(L):
     l_exclude_text = ''
 
 
-    if L <= 0.90 and str(ACC_ID) != '3':
-            l_exclude_text = l_exclude_text + 'Вилка ' + str(L) + ' (' + str(round((1 - L) * 100, 2)) + '%), вилка исключена т.к. доходноть высокая >= 10%\n'
+    # if L <= 0.90 and str(ACC_ID) != '3':
+    #         l_exclude_text = l_exclude_text + 'Вилка ' + str(L) + ' (' + str(round((1 - L) * 100, 2)) + '%), вилка исключена т.к. доходноть высокая >= 10%\n'
     if L > MIN_L:
         l_exclude_text = l_exclude_text + 'Вилка ' + \
                          str(L) + ' (' + str(round((1 - L) * 100, 3)) + '%), беру вилки только >= ' + str(round((1 - MIN_L) * 100, 3)) + '%\n'
