9ad301e38458480879de49d8798b444e6fd6a720
suineg <suineg@inbox.ru>
2019-08-23 09:59:59 +0500

    change key switch acc_id

--- utils.py
+++ utils.py
@@ -72,12 +72,13 @@ USER_ID = 0
 PROPERTIES = {}
 ACCOUNTS = {}
 try:
-    KEY = sys.argv[2]
+    acc_id = sys.argv[2]
 except:
     pass
 
-if KEY:
-    acc_info = Account.select().where(Account.key == KEY)
+if acc_id:
+    acc_info = Account.select().where(Account.id == acc_id)
+    KEY = ACC_ID = acc_info.get().key
     if acc_info:
         print('___________________INIT____________________')
         ACCOUNTS = loads(acc_info.get().accounts.replace('`', '"'))
