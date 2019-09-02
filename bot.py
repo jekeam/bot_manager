3a477c24b66296bc226028b8fc85f2d61d0a4687
suineg <suineg@inbox.ru>
2019-09-02 08:48:58 +0500

    bfx calc sale bet

--- bot.py
+++ bot.py
@@ -86,8 +86,7 @@ def print_stat(acc_id: str) -> str:
             res_str = ''
             res_str = res_str + 'Проставлено вилок: *' + str(cnt_fork_success) + '*\n'
             res_str = res_str + 'Кол-во минусовых выкупов: *' + str(cnt_fail) + '*\n'
-            res_str = res_str + 'Минимальный профит: *' + '{:,}'.format(round(min_profit)).replace(',', ' ') + '*\n'
-            res_str = res_str + 'Максимальный профит: *' + '{:,}'.format(round(max_profit)).replace(',', ' ') + '*\n'
+            res_str = res_str + 'Профит от ставок: *' + '{:,}-{:,}'.format(round(min_profit), round(max_profit)).replace(',', ' ') + '*\n'
             res_str = res_str + 'Профит от выкупов: *' + '{:,}'.format(round(sale_profit)).replace(',', ' ') + '*\n'
             res_str = res_str + '\n*Примерный доход: ' + '{:,}'.format(round((max_profit + min_profit) / 2) + round(sale_profit)).replace(',', ' ') + '*\n'
 
