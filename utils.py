3208efdcd461d1acd634ca20442ff84ae016a2fe
suineg <suineg@inbox.ru>
2019-09-23 15:31:03 +0500

    add msg about lock proxy

--- utils.py
+++ utils.py
@@ -319,6 +319,8 @@ def check_status_with_resp(resp, olimp=None):
     if (resp.status_code != 200 and olimp is None) \
             or (olimp is not None and resp.status_code not in (200, 400, 417, 406, 403, 500)):
         raise LoadException("Site is not responding, status code: {}".format(resp.status_code))
+    if '149-ФЗ'.lower() in resp.text.lower():
+        raise LoadException('Прокси при запросе к ' + str(resp.url) + ', был заблокирован на основании 149-ФЗ, нужно сменить прокси.')
 
 
 def get_session_with_proxy(name):
