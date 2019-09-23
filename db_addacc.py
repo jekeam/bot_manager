42bf91cce00390d17f19e1ea7f36a115c645305e
suineg <suineg@inbox.ru>
2019-09-23 19:34:43 +0500

    Update db_addacc.py

--- db_addacc.py
+++ db_addacc.py
@@ -31,21 +31,24 @@ if __name__ == '__main__':
     proxies = '{`fonbet`:{`http`:`http://' + proxy + '`,`https`:`https://' + proxy + '`},`olimp`:{`http`:`http://' + proxy + '`,`https`:`https://' + proxy + '`}}'
     accounts = '{`olimp`:{`login`:`' + olu + '`,`password`:`' + olp + '`,`mirror`:`olimp.com`},`fonbet`:{`login`:' + fbu + ',`password`:`' + fbp + '`,`mirror`:`fonbet.com`}}'
     try:
-        acc = Account.create(
-            # user id from telegram
-            user=304415738,
-            key=uid,
-            date_end=get_trunc_sysdate(30),  # cnt days
-            status='active',
-            proxies=proxies,
-            accounts=accounts
+        # acc = Account.create(
+        #     # user id from telegram
+        #     user=304415738,
+        #     key=uid,
+        #     date_end=get_trunc_sysdate(30),  # cnt days
+        #     status='active',
+        #     proxies=proxies,
+        #     accounts=accounts
+        # )
+
+        # print(acc.id)
+
+        prop = (
+            Properties.insert_from(
+                Properties.select(41, Properties.val, Properties.key).where(Properties.acc_id == 18),
+                fields=[Properties.acc_id, Properties.val, Properties.key]
+            ).execute()
         )
-
-        print(acc.id)
-
-        Properties.insert_from(
-            Properties.select(Properties.val, Properties.key).where(Properties.acc_id == 18),
-            [acc.id, Properties.val, Properties.key]).execute()
     except Exception as e:
         print(e)
     print('OK')
