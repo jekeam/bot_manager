a59b8dba9bb68d7409a50c734e94cdb99ad58a9f
suineg <suineg@inbox.ru>
2019-09-23 20:13:58 +0500

    Update bet_olimp.py

--- bet_olimp.py
+++ bet_olimp.py
@@ -83,11 +83,14 @@ class OlimpBot:
             )
             prnt('BET_OLIMP.PY: Olimp, sign_in responce: ' + str(resp.status_code) + ' ' + resp.text, 'hide')
 
-            check_status_with_resp(resp, 'olimp')
+            try:
+                res = resp.json()
+                if res.get('error', {}).get('err_code', 0) == 401:
+                    raise ValueError('Olimp:' + res.get('error', {}).get('err_desc'))
+            except Exception as e:
+                prnt('BET_OLIMP.PY: ' + str(e))
 
-            res = resp.json()
-            if res.get('error', {}).get('err_code', 0) == 401:
-                raise ValueError('Olimp:' + res.get('error', {}).get('err_desc'))
+            check_status_with_resp(resp)
 
             self.session_payload["session"] = res["data"]["session"]
             login_info = dict(res['data'])
