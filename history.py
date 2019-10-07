0d8717149134c4370b2ae6a13f3d52d7d3e4231b
suineg <suineg@inbox.ru>
2019-10-08 01:21:32 +0500

    add top/hot to export

--- history.py
+++ history.py
@@ -229,8 +229,9 @@ def export_hist(OLIMP_USER, FONBET_USER):
                           str(info['olimp'].get('balance', '')) + ';' + \
  \
                           str(info['fonbet'].get('max_bet', '')) + ';' + \
- \
                           str(info['fonbet'].get('bet_delay', '')) + ';' + \
+                          str(info['fonbet'].get('is_top', '')) + ';' + \
+                          str(info['fonbet'].get('is_hot', '')) + ';' + \
  \
                           str(round((1 - info['fonbet'].get('l', 0.0)) * 100, 3)) + ';' + \
  \
@@ -243,7 +244,7 @@ def export_hist(OLIMP_USER, FONBET_USER):
                          'fb_id;o_id;fb_time;o_time;fb_kof;o_kof;fb_sum_bet;o_sum_bet;' \
                          'fb_profit;o_profit;fb_result;o_result;fb_name;o_name;fb_status;' \
                          'o_sum_sale;f_kof_type;o_kof_type;fb_vector;ol_vector;fb_time_bet;ol_time_bet;' \
-                         'fb_new_bet_sum;ol_new_bet_sum;fb_bal;ol_bal;fb_max_bet;fb_bet_delay;cur_proc;fisrt_proc;fb_err;ol_err;\n'
+                         'fb_new_bet_sum;ol_new_bet_sum;fb_bal;ol_bal;fb_max_bet;fb_bet_delay;fb_is_top;fb_is_hot;cur_proc;fisrt_proc;fb_err;ol_err;\n'
 
             with open(csv_name, 'w', encoding='utf-8') as f:
                 f.write(header + out)
