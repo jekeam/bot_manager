246043514ba8b3fbe67021d89312112404f226aa
Ubuntu <suinegne@gmail.com>
2019-11-14 07:51:50 +0000

    test

--- better.py
+++ better.py
@@ -584,6 +584,7 @@ def run_client():
         msg_err = 'run_client: ' + str(e.__class__.__name__) + ' - ' + str(e)
         prnt(str(e.__class__.__name__) + ' - ' + msg_err)
         prnt(time_out_cnt)
+        prnt(send_time_out)
         server_forks = {}
         conn.close()
         
@@ -591,6 +592,8 @@ def run_client():
             time_out_cnt = time_out_cnt + 1
             if time_out_cnt > 3:
                 for admin in ADMINS:
+                    prnt(admin)
+                    prnt(str(ACC_ID) + ': Возникла ошибка при запросе катировок со сканнера, просьба презапустить сканнер, ' + str(msg_err))
                     send_message_bot(admin, str(ACC_ID) + ': Возникла ошибка при запросе катировок со сканнера, просьба презапустить сканнер, ' + str(msg_err))
                 send_time_out = True
         
