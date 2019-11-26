a0cac6e877e4484894523643d385e9c401d0ea5e
Ubuntu <suinegne@gmail.com>
2019-11-26 07:24:12 +0000

    add log

--- bot_starter.py
+++ bot_starter.py
@@ -10,16 +10,24 @@ from threading import Thread
 import subprocess
 import os
 import time
+from random import uniform
+
+
+def prntbs(vstr, filename='bot_starter.log'):
+    Outfile = open(filename, "a+", encoding='utf-8')
+    Outfile.write(vstr + '\n')
+    Outfile.close()
+
 
 if __name__ == '__main__':
     def start(acc_id: str, wait_sec: str=''):  # abs_path:str
         # os.chdir(abs_path)
         if os.path.isfile('better.py'):
             call_str = bot_prop.PY_PATH + ' better.py --acc_id ' + str(acc_id) + ' ' + wait_sec
-            print('dir: ' + str(os.getcwd()) + ', command: ' + call_str)
+            prntbs('dir: ' + str(os.getcwd()) + ', command: ' + call_str)
             subprocess.call(call_str, shell=True)
         else:
-            print('file better.py not found in ' + str(os.getcwd()))
+            prntbs('file better.py not found in ' + str(os.getcwd()))
 
 
     if __name__ == '__main__':
@@ -27,12 +35,15 @@ if __name__ == '__main__':
         while True:
             for acc in Account.select().where((Account.status == 'active') & ((Account.work_stat == 'start_sleep')|(Account.work_stat == 'start')) & (Account.pid == 0)):
                 Account.update(pid=1).where(Account.id == acc.id).execute()
-                print(''.ljust(120, '*'))
+                prntbs(''.ljust(120, '*'))
+                random_time = uniform(0, 1)
+                prntbs('random_time: ' + str(random_time))
+                time.sleep(random_time)
                 if acc.work_stat == 'start':
-                    print('start: ', acc.id)  # acc.work_dir,
+                    prntbs('start: ' + str(acc.id))  # acc.work_dir,
                     acc_start = Thread(target=start, args=(acc.id,))  # acc.work_dir,
                 else:
-                    print('start_sleep: ', acc.id)  # acc.work_dir,
+                    prntbs('start_sleep: ' + str(acc.id))  # acc.work_dir,
                     acc_start = Thread(target=start, args=(acc.id, 'start_sleep'))  # acc.work_dir,
                 acc_start.start()
             time.sleep(5)
