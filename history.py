dc25c215d5a9cca3c9ff94bb436b975c5a3b9195
Ubuntu <suinegne@gmail.com>
2019-12-18 11:52:26 +0000

    bfx

--- history.py
+++ history.py
@@ -294,7 +294,7 @@ def export_hist(OLIMP_USER, FONBET_USER):
             if not os.path.isfile(new_file_name_hide_log):
                 os.rename(file_name_log, new_file_name_hide_log)
             else:
-                os.rename(file_name_log, new_file_name_hide_log.replace('.', str(datetime.now().strftime('%H_%M')) + '.'))
+                os.rename(file_name_log, new_file_name_hide_log.replace('.', datetime.now().strftime('%H_%M') + '.'))
         except Exception as e:
             prnt(e)
 
@@ -305,7 +305,7 @@ def export_hist(OLIMP_USER, FONBET_USER):
             if not os.path.isfile(new_file_name_cl_bet_log):
                 os.rename(file_name_log, new_file_name_cl_bet_log)
             else:
-                os.rename(file_name_log, new_file_name_cl_bet_log.replace('.', str(datetime.now().strftime('%H_%M')) + '.'))
+                os.rename(file_name_log, new_file_name_cl_bet_log.replace('.', datetime.now().strftime('%H_%M') + '.'))
         except Exception as e:
             prnt(e)
 
@@ -316,7 +316,7 @@ def export_hist(OLIMP_USER, FONBET_USER):
             if not os.path.isfile(new_file_name_cl_fork_log):
                 os.rename(file_name_log, new_file_name_cl_fork_log)
             else:
-                os.rename(file_name_log, new_file_name_cl_fork_log.replace('.', str(datetime.now().strftime('%H_%M')) + '.'))
+                os.rename(file_name_log, new_file_name_cl_fork_log.replace('.', datetime.now().strftime('%H_%M') + '.'))
         except Exception as e:
             prnt(e)
 
