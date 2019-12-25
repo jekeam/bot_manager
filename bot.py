615344227b4b4f5095d3b0b2fedc3c9f8489d559
Ubuntu <suinegne@gmail.com>
2019-12-25 06:55:19 +0000

    bfzx

--- bot.py
+++ bot.py
@@ -289,12 +289,10 @@ def choose_prop(update, context):
             if val.get('min'):
                 str_r = str_r + ', max: ' + val.get('max')
             dop_indo = str_r
-            print('val.get(type)' + str(val.get('type')))
-            print('access_list' + str(val.get('access_list')))
-            print('dop_indo' + str(dop_indo))
+            
             if val.get('access_list'):
                 if dop_indo:
-                    dop_indo + ', '
+                    dop_indo = dop_indo + ', '
                 dop_indo = 'допустимые значения: ' + str(val.get('access_list')).replace("'", '')
             elif 'proxi:' in val.get('type'):
                 proxy = val.get('type').split(':')[1]
