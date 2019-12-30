481b093a9c7b93335acf3d0312310330a10c8624
root <root@airat.sharipov.fvds.ru>
2019-12-30 12:34:49 +0300

    bfx

--- better.py
+++ better.py
@@ -430,7 +430,7 @@ def go_bets(wag_ol, wag_fb, key, deff_max, vect1, vect2, sc1, sc2, created, even
                 data=data[data.val.apply(len)>1]
                 data=data.reset_index(drop=True)
                 vect = str(ml.preprocessing(data.sec[0], data.val[0], True, str(ACC_ID), str(fork_id)))
-                prnt('fork_id: {}, vect: {}'.fromat(fork_id, vect))
+                prnt('fork_id: {}, vect: {}'.format(fork_id, vect))
                 if vect.lower() != 'up'.lower():
                     prnt('Проверка на ML не пройдена т.к. вектор в олимп не UP')
                     return False
