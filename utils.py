61f86c9a7d41dc44dc3644d28140411c83c86747
Ubuntu <ubuntu@ip-172-31-24-151.us-east-2.compute.internal>
2020-02-06 04:18:45 +0000

    bfx

--- utils.py
+++ utils.py
@@ -194,8 +194,9 @@ def get_sum_bets(k1, k2, total_bet, round_fork=5, hide=False, debug=False):
     l = (1 / k1) + (1 / k2)
 
     # Округление проставления в БК1 происходит по правилам математики
-    bet_1 = round(total_bet / (k1 * l) / round_fork) * round_fork
-    bet_2 = total_bet - bet_1
+    bet_1 = floor(total_bet / (k1 * l) / round_fork) * round_fork
+    # bet_2 = total_bet - bet_1
+    bet_2 = floor(total_bet / (k2 * l) / round_fork) * round_fork
     if debug:
         prnt('L: ' + str(round((1 - l) * 100, 2)) + '% (' + str(l) + ') ', hide)
         prnt('bet1: ' + str(bet_1) + ', bet2: ' + str(bet_2) + '|' + ' bet_sum: ' + str(bet_1 + bet_2) + '\n', hide)
