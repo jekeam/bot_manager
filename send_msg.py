d6b8f161811d77e77e8e1c2c477cf6a32fcc6107
Alexander Savinykh <suinegne@gmail.com>
2020-03-27 21:53:32 +0500

    Update send_msg.py

--- send_msg.py
+++ send_msg.py
@@ -4,9 +4,12 @@ import db_model
 import bot_prop
 print(sys.argv)
 msg = sys.argv[1]
+for_admin = sys.argv[2]
 channels_id = ['-1001334950920', ]
 if msg:
-    # for admin in bot_prop.ADMINS:
-    #     db_model.send_message_bot(admin, msg)
-    for id in channels_id:
-        db_model.send_message_bot(id, msg)
+    if for_admin == 'True':
+        for admin in bot_prop.ADMINS:
+            db_model.send_message_bot(admin, msg)
+    else:
+        for id in channels_id:
+            db_model.send_message_bot(id, msg)
